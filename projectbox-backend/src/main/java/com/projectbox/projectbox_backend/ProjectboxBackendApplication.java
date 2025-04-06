07a58270231bbe89782d0eab773000fd78193656
ThePerformer <fjimmywilson@gmail.com>
2025-04-06 19:19:51 +0100

    backend updated

---
+++ projectbox-backend/src/main/java/com/projectbox/projectbox_backend/ProjectboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projectbox.projectbox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectboxBackendApplication.class, args);
+	}
+
+}
